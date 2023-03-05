1648f955d5e9da0017fa41efb71b14813f7f5edd
dnaux <umitsevincler99@gmail.com>
2023-03-06 00:22:04 +0300

    Changed file structure, added button functionalities

--- src/app.js
+++ src/app.js
@@ -1,9 +1,43 @@
-var stations = document.getElementsByClassName('station');
+import data from './edges.json' assert { type: 'json' };
+const stations = document.getElementsByClassName('station');
+const text_start = document.getElementById('from');
+const text_end = document.getElementById('to');
+const text_eta = document.getElementById('eta');
+const text_cur = document.getElementById('cur_selected');
+const btn_start = document.getElementById('set_from');
+const btn_end = document.getElementById('set_to');
+
+console.log(data);
+
+let cur = NaN;
+let start = NaN;
+let end = NaN;
 
 for(let i = 0; i < stations.length; i++) {
-    stations[i].addEventListener('click', alertcall.bind(null, i));
+    stations[i].addEventListener('click', function() {
+        cur = i;
+        text_cur.innerHTML = 'Selected: ' + cur;
+    });
 }
 
-function alertcall(x) {
-    window.alert(x);
+btn_start.addEventListener('click', function(){
+    start = cur;
+    text_start.innerHTML = 'From: ' + start;
+
+    if(start != NaN && end != NaN) {
+        calculate_dist(start, end);
+    }
+});
+
+btn_end.addEventListener('click', function(){
+    end = cur;
+    text_end.innerHTML = 'To: ' + end;
+
+    if(start != NaN && end != NaN) {
+        calculate_dist(start, end);
+    }
+});
+
+function calculate_dist(start, end) {
+
 }
