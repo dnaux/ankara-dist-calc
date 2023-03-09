6da88c6441525ca4554956ca111e82660284f557
dnaux <umitsevincler99@gmail.com>
2023-03-09 12:43:01 +0300

    Fixes and new stuff

--- src/app.js
+++ src/app.js
@@ -6,22 +6,37 @@ const text_cur = document.getElementById('cur_selected');
 const btn_start = document.getElementById('set_from');
 const btn_end = document.getElementById('set_to');
 
-let response = await fetch('/src/edges.json')
+let response = await fetch('src/edges.json')
 let graph = await response.json();
 let cur = NaN;
 let start = NaN;
 let end = NaN;
 
+function change_select(sel = -1) {
+    if(sel >= 0) {
+        text_cur.innerHTML = 'Selected: ' + graph.stations[sel];
+    }
+    else {
+        cur = -1;
+        text_cur.innerHTML = 'Selected: None';
+    }
+}
+
 for(let i = 0; i < stations.length; i++) {
     stations[i].addEventListener('click', function() {
         cur = i;
-        text_cur.innerHTML = 'Selected: ' + cur;
+        change_select(i);
     });
 }
 
 btn_start.addEventListener('click', function(){
+
+    if(cur == -1)
+        return;
+    
     start = cur;
-    text_start.innerHTML = 'From: ' + start;
+    text_start.innerHTML = 'From: ' + graph.stations[start];
+    change_select();
 
     if(start != NaN && end != NaN) {
         calculate_dist(start, end, 0, -1);
@@ -29,8 +44,13 @@ btn_start.addEventListener('click', function(){
 });
 
 btn_end.addEventListener('click', function(){
+
+    if(cur == -1)
+        return;
+
     end = cur;
-    text_end.innerHTML = 'To: ' + end;
+    text_end.innerHTML = 'To: ' + graph.stations[end];
+    change_select();
 
     if(start != NaN && end != NaN) {
         calculate_dist(start, end, 0, -1);
@@ -39,7 +59,7 @@ btn_end.addEventListener('click', function(){
 
 function calculate_dist(cur, end, sum, par) {
     if(cur == end) {
-        text_eta.innerHTML = 'ETA: ' + sum;
+        text_eta.innerHTML = 'ETA: ' + sum + ' minutes';
         return;
     }
 
